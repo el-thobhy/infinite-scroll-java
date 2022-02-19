0292ac7f7b671cbe85b16dca51a07bf8022f55bf
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-02-20 02:55:06 +0700

    Infinite scroll dengan API Movie DB

---
+++ InfiniteScrollApi/app/build/generated/source/buildConfig/debug/com/elthobhy/infinitescrollapi/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.elthobhy.infinitescrollapi;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.elthobhy.infinitescrollapi";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String API_VERSION = "3/";
+  // Field from default config.
+  public static final String BASE_URL_IMAGE_MOVIE_DB = "https://image.tmdb.org/t/p/w500/";
+  // Field from default config.
+  public static final String BASE_URL_MOVIE_DB = "https://api.themoviedb.org/";
+  // Field from default config.
+  public static final String TOKEN_MOVIE_DB = "dccd44dbfc2c7c82f7c3b46080a96b16";
+}
